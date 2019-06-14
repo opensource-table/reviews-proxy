09f49aa0aaa277dd6cce123b79f8c218229aa8a7
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-06-13 18:59:49 -0700

    Set up k6 script for proxy server.

---
+++ k6.js
@@ -0,0 +1,68 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+
+export let options = {
+  stages: [
+    {duration: "15s", target: 10},
+    {duration: "15s", target: 10},
+  ]
+};
+
+let min = 8000000;
+let max = 10000000;
+const fakeText = ['dksalfjdasl ewqr d aic aewqr aka da', 'afkldfq caskl qeri das fc al', 'jdakl feqlk fda ca ewq', 'fjdkal eq pc qipurexnm'];
+const fakeDates = ['2019-01-01', '2018-10-10', '2019-04-04', '2019-05-01'];
+const fakeBooleans = ['t', 'f'];
+
+export default function() {
+  let decidePostOrGet = Math.random();
+
+  if (decidePostOrGet > 0.95) {
+    let randNum = Math.random();
+    let formData = {
+      restaurant_id: Math.floor(randNum*((max-min) + min)),
+      user_id: Math.floor(randNum * 5000000),
+      date: fakeDates[Math.floor(randNum * 4)],
+      text: fakeText[Math.floor(randNum * 4)],
+      overall_score: Math.floor(randNum * 4) + 1,
+      food_score: Math.floor(randNum * 4) + 1,
+      service_score: Math.floor(randNum * 4) + 1,
+      ambience_score: Math.floor(randNum * 4) + 1,
+      value_score: Math.floor(randNum * 4) + 1,
+      is_recommended: fakeBooleans[Math.floor(randNum * 2)],
+      tags: 'gluten free/dairy free/wine'
+    }
+
+    let headers = { "Content-Type": "application/x-www-form-urlencoded" }
+    if (randNum > 0.8) {
+      let res = http.post(`http://localhost:3000/${Math.floor(Math.random()*((max-min) + min))}`, formData, { headers: headers });
+      check(res, {
+        "status was 201": (r) => r.status == 201,
+        "transaction time OK": (r) => r.timings.duration < 100
+      });
+    } else {
+      let res = http.post(`http://localhost:3000/${Math.floor(Math.random())}`, formData, { headers: headers });
+      check(res, {
+        "status was 201": (r) => r.status == 201,
+        "transaction time OK": (r) => r.timings.duration < 100
+      });
+    }
+    sleep(1);
+  } else {
+    let randNum = Math.random();
+    if (randNum > 0.8) {
+      let res = http.get(`http://localhost:3000/${Math.floor(Math.random()*((max-min) + min))}`);
+      check(res, {
+        "status was 200": (r) => r.status == 200,
+        "transaction time OK": (r) => r.timings.duration < 100
+      });
+    } else {
+      let res = http.get(`http://localhost:3000/${Math.floor(Math.random())}`);
+      check(res, {
+        "status was 200": (r) => r.status == 200,
+        "transaction time OK": (r) => r.timings.duration < 100
+      });
+    }
+    sleep(1);
+  }
+};
