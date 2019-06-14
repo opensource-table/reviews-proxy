4536262d0baef8ed2518164dd5478ea9aa105fcb
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-06-13 18:23:44 -0700

    Remove unnecessary server code.

--- server/index.js
+++ server/index.js
@@ -6,15 +6,6 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, '../public')));
 app.use('/:id', proxy({target: 'http://localhost:3010/', changeorigin: true}));
 
-// app.get('/:id', (req, res) => {
-//   if (!req.params.id) {
-//     res.status(400);
-//     res.end();
-//   } else {
-//     res.sendFile('index.html', { root: path.resolve(__dirname, '../public') });
-//   }
-// });
-
 app.listen(3000, () => {
   console.log('Open Table proxy server listening on port 3000!');
 });
