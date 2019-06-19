7905c839cbe9ee9f4a460cc5980d320caedd13dd
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-06-19 10:43:55 -0700

    Update url for module EC2 instance

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../public')));
-app.use('/:id', proxy({target: 'http://localhost:3010/', changeorigin: true}));
+app.use('/:id', proxy({target: 'http://18.221.67.249:3010/', changeorigin: true}));
 
 app.listen(3000, () => {
   console.log('Open Table proxy server listening on port 3000!');
